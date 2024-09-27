77d26210a335c1d1aba444bed8291b29cc73bfdf
Siftab <siiffuuu@gmail.com>
2024-09-27 11:06:37 +0600

    initializing

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+  <div>
+     <h1>my todo  </h1>
+     <Button> shadcn</Button>
+  </div>
+  )
+}
+
+export default App
