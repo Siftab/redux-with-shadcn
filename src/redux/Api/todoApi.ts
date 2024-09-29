3a5b1a94fe8ae690cf984d8be2ce0e511e57293a
Siftab <siiffuuu@gmail.com>
2024-09-29 20:36:53 +0600

    feat(todoApi):basic structre for reduxt todo Api

---
+++ src/redux/Api/todoApi.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+
+
+export const todoApi = createApi({
+    reducerPath:"todoApi",
+    baseQuery:fetchBaseQuery({baseUrl:'http://localhost:5000/tasks'}),
+    endpoints:(builder)=>({
+        getTodos: builder.query({
+            query:()=>({
+                url:'/tasks',
+            method:"GET"
+            })
+        })
+    })
+})
