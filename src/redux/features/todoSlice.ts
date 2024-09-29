0dc0044e196d1cb12e71745afc5b81d2b225bd73
Siftab <siiffuuu@gmail.com>
2024-09-29 20:49:29 +0600

    config(api to store):configartion with store

--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -1,4 +1,3 @@
-import { Item } from "@radix-ui/react-dropdown-menu";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from './features/todoSlice'
+import { todoApi } from "./Api/todoApi";
 
 
 
 
 export const reduxStore = configureStore({
     reducer:{
+        [todoApi.reducerPath]:todoApi.reducer
+        ,
         todo: todoReducer
 
-    }
+    },
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(todoApi.middleware)
 })
 
 
